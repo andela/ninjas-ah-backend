ed78171f9f25201019f1b6308b5f860d8fdb9d78
Benedicte Musabimana <benedicte.musabimana@Benedictes-MacBook-Pro.local>
2019-05-02 21:54:16 +0200

    [feature 165412922] Change database configuration

--- src/models/index.js
+++ src/models/index.js
@@ -7,18 +7,16 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 
 const db = {};
-let sequelize;
 const config = dbConfig[env];
 
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = config.use_env_variable
+  ? new Sequelize(process.env[config.use_env_variable], config)
+  : new Sequelize(config.database, config.username, config.password, config);
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+fs.readdirSync(__dirname)
+  .filter(
+    file => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+  )
   .forEach((file) => {
     const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
