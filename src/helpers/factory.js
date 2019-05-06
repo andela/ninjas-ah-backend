9b73ea83991395116249a18178dc3c976f6d1c7c
Benedicte Musabimana <benedicte.musabimana@Benedictes-MacBook-Pro.local>
2019-05-06 12:30:53 +0200

    [feature 165412922] Create dummy data

---
+++ src/helpers/factory.js
@@ -0,0 +1,12 @@
+// // eslint-disable-next-line import/no-extraneous-dependencies
+// import Factory from 'rosie';
+
+// Factory.define('Comment')
+//   .sequence('id')
+//   .attr('articleId', '1')
+//   .attr('userId', '1')
+//   .attr('body', 'Body Factory')
+//   .attr('createdAt', () => new Date())
+//   .attr('updatedAt', () => new Date());
+
+// export default Factory;
