a2142cc2690ceae621e767becf3100e46de5fd1b
MBenedicte <mbenedicte20@gmail.com>
2019-05-09 07:37:47 +0200

    [Feature 165412922] Add comments exnpoints

---
+++ src/helpers/queryHelper.js
@@ -0,0 +1,28 @@
+const dbFindSingle = async (model, whereCondition) => model.findOne({
+  where: whereCondition
+});
+
+const dbFindAll = async (model, whereCondition) => model.findAll({
+  where: whereCondition
+});
+
+const dbCreate = async (model, condition) => model.create(condition);
+
+const dbDelete = async (model, whereCondition) => model.destroy({
+  where: whereCondition
+});
+
+const dbUpdate = async (model, condition, whereCondition) => model.update(
+  { condition },
+  {
+    where: whereCondition
+  }
+);
+
+module.exports = {
+  dbFindSingle,
+  dbFindAll,
+  dbCreate,
+  dbDelete,
+  dbUpdate
+};
---
+++ src/queries/articles/oneArticle.js
@@ -0,0 +1,5 @@
+import db from '../../models';
+import { dbFindSingle } from '../../helpers/queryHelper';
+
+const getOne = async (condition = {}) => dbFindSingle(db.Article, condition);
+module.exports = { getOne };
