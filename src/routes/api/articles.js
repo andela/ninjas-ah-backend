be5dc38d7a263376e7018282a120769d62f16d26
Gilles Kagarama <kagaramag@gmail.com>
2019-05-11 13:11:31 +0200

    [feature #165412917] rename async handler

--- src/routes/api/articles.js
+++ src/routes/api/articles.js
@@ -1,44 +1,44 @@
 import { Router } from 'express';
 import ArticleController from '../../controllers/ArticlesController';
 import validateArticle from '../../middlewares/validation/articles';
-import errorCatcher from '../../middlewares/errorCatcher';
+import asyncHandler from '../../middlewares/asyncHandler';
 import checkArticle from '../../middlewares/checkArticle';
 
 const articles = Router();
 // create article
-articles.post('/articles/', validateArticle.create, errorCatcher(ArticleController.saveArticle));
-articles.get('/articles/', errorCatcher(ArticleController.getAllArticles));
-articles.get('/articles/:slug', checkArticle, errorCatcher(ArticleController.getSpecificArticle));
+articles.post('/articles/', validateArticle.create, asyncHandler(ArticleController.saveArticle));
+articles.get('/articles/', asyncHandler(ArticleController.getAllArticles));
+articles.get('/articles/:slug', checkArticle, asyncHandler(ArticleController.getSpecificArticle));
 articles.put(
   '/articles/:slug',
   validateArticle.update,
   checkArticle,
-  errorCatcher(ArticleController.update)
+  asyncHandler(ArticleController.update)
 );
 
 articles.put(
   '/articles/:slug/publish',
   validateArticle.slug,
   checkArticle,
-  errorCatcher(ArticleController.update)
+  asyncHandler(ArticleController.update)
 );
 articles.put(
   '/articles/:slug/publish',
   validateArticle.slug,
   checkArticle,
-  errorCatcher(ArticleController.update)
+  asyncHandler(ArticleController.update)
 );
 articles.put(
   '/articles/:slug/unpublish',
   validateArticle.slug,
   checkArticle,
-  errorCatcher(ArticleController.update)
+  asyncHandler(ArticleController.update)
 );
 articles.delete(
   '/articles/:slug',
   checkArticle,
   validateArticle.slug,
-  errorCatcher(ArticleController.update)
+  asyncHandler(ArticleController.update)
 );
 
 export default articles;
