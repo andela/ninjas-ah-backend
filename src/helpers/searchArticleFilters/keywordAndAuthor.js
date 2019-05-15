26be74f6d8cce8224805f7aaf8910db16e342959
Gilles Kagarama <kagaramag@gmail.com>
2019-05-16 00:27:36 +0200

    [feature #165412931] filter articles by keyword and author

--- src/helpers/searchArticleFilters/keywordAndAuthor.js
+++ src/helpers/searchArticleFilters/keywordAndAuthor.js
@@ -1,19 +1,18 @@
 import db from '../../models';
 
-export default (keyword, author, tag) => {
-  console.log('keyword and author');
-  let where = {};
-  if (author || keyword || tag) {
-    where = {
-      status: { [db.Op.ne]: 'deleted' },
+export default (keyword, author) => {
+  const where = {
+    status: { [db.Op.ne]: 'deleted' },
+    [db.Op.and]: {
+      title: {
+        [db.Op.iLike]: `%${keyword}%`
+      },
       [db.Op.or]: [
-        {
-          title: {
-            [db.Op.iLike]: `%${keyword}%`
-          }
-        }
+        { '$author.username$': { [db.Op.iLike]: `${author}` } },
+        { '$author.firstName$': { [db.Op.iLike]: `${author}` } },
+        { '$author.lastName$': { [db.Op.iLike]: `${author}` } }
       ]
-    };
-  }
+    }
+  };
   return where;
 };
