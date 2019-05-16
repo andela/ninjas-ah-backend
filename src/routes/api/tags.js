c7af76ff6d021b46bebe5a6b973abc83235e4748
Gilles Kagarama <kagaramag@gmail.com>
2019-05-16 10:52:27 +0200

    [feature #165412927] add middleware to validate tags

--- src/routes/api/tags.js
+++ src/routes/api/tags.js
@@ -1,12 +1,20 @@
 import { Router } from 'express';
 import TagController from '../../controllers/TagController';
 import asyncHandler from '../../middlewares/asyncHandler';
+import validateArticle from '../../middlewares/validation/articles';
 import checkArticleBySlug from '../../middlewares/checkArticleBySlug';
+import validateTags from '../../middlewares/validation/validateTags';
 
 const tags = Router();
 
 // create article
-tags.put('/articles/:slug/tags', checkArticleBySlug, asyncHandler(TagController.update));
+tags.put(
+  '/articles/:slug/tags',
+  checkArticleBySlug,
+  validateTags.create,
+  validateArticle.slug,
+  asyncHandler(TagController.update)
+);
 tags.delete('/articles/:slug/tags', checkArticleBySlug, asyncHandler(TagController.update));
 tags.get('/tags', asyncHandler(TagController.getAll));
 
