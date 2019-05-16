9e83bf7e6949e3de2dfbd228a129c4f8161b5620
jean.luctuyishime <luctunechi45@gmail.com>
2019-05-16 12:49:54 +0200

    [ft-165412939] add role base functionalities

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,9 +1,11 @@
 import * as User from './users';
 import * as Article from './articles';
+import createUser from './users/createUser';
 import * as Token from './tokens/index';
 import * as Tag from './tags';
 import * as Chat from './chats';
+import * as ArticleBookmark from './articleBookmarks';
 
 export {
-  User, Article, Token, Tag, Chat
+  User, Article, Token, Tag, ArticleBookmark, createUser, Chat
 };
--- src/routes/api/roles.js
+++ src/routes/api/roles.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import UserController from '../../controllers/UserController';
 import verifyToken from '../../middlewares/verifyToken';
 import checkUpdateUserPermission from '../../middlewares/checkUpdateUserPermission';
+import verifyAdminUser from '../../middlewares/verifyAdmin';
 
 const rolesRouter = Router();
 
@@ -9,6 +10,7 @@ rolesRouter.put(
   '/:username',
   verifyToken,
   checkUpdateUserPermission,
+  verifyAdminUser,
   UserController.updateUserRole
 );
 
