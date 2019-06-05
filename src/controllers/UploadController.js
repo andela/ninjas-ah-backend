f31179bbfa3c1f65972a98f29408d1a2d058ff98
Gilles Kagarama <kagaramag@gmail.com>
2019-06-05 11:43:19 +0200

    --amend

--- src/controllers/UploadController.js
+++ src/controllers/UploadController.js
@@ -13,15 +13,29 @@ export default class UploadController {
    */
   static async save(req, res) {
     const response = req.file && (await upload(req));
-    return typeof response === 'object'
+    return typeof response === 'object' && typeof response !== 'boolean' && response !== null
       ? (await Gallery.save({ image: response.image.original, userId: req.user.id }))
           && res.status(status.CREATED).json({
             response
           })
+<<<<<<< HEAD
       : res.status(!req.file ? status.BAD_REQUEST : status.SERVER_ERROR).json({
         errors: {
           image: 'Whoops, something went wrong while uploading your image. try again!'
         }
       });
+=======
+      : (!req.file
+          && res.status(status.BAD_REQUEST).json({
+            errors: {
+              image: 'sorry, you did not select image to be uploaded'
+            }
+          }))
+          || res.status(status.SERVER_ERROR).json({
+            errors: {
+              image: 'sorry, your file was not uploaded'
+            }
+          });
+>>>>>>> --amend
   }
 }
